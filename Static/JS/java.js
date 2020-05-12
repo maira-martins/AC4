fa58f2a1791f4ea1ef1aae0acd234b0db8bc9582
unknown <felipe1793@hotmail.com>
2020-05-11 22:03:02 -0300

    Fim

--- Static/JS/java.js
+++ Static/JS/java.js
@@ -20,6 +20,6 @@ for (var controladora = 1; controladora <= 10; controladora++){
     c.appendChild(t);
 
     var p = document.createElement('p');
-    p.textContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+    p.textContent = 'Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor, que serão interpretadas pelo leitor de acordo com seus conhecimentos linguísticos e culturais.';
     c.appendChild(p);
 }
